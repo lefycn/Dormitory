1d7e2812e144493c5b37b69cbeab79a95bba55bf
王飞 <f1570053668@163.com>
2017-10-25 21:43:03 +0800

    登录界面

--- app/src/main/java/cn/edu/pku/dormitory/MainActivity.java
+++ app/src/main/java/cn/edu/pku/dormitory/MainActivity.java
@@ -1,13 +1,30 @@
 package cn.edu.pku.dormitory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button myLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+    }
+
+    private void init() {
+        myLogin = (Button) findViewById(R.id.login);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.login){
+            Intent intent = new Intent(this,LoginActivity.class);
+        }
     }
 }
